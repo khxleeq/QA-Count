3ba5343281c3e1f3667ce66c88b7161eb2438c80
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-04 14:09:27 +0100

    InitialMsg

---
+++ index.js
@@ -0,0 +1,34 @@
+const minus1 = document.getElementById('minus-1')
+const minus5 = document.getElementById('minus-5')
+const reset = document.getElementById('R')
+const add1 = document.getElementById('add+1')
+const add5 = document.getElementById('add+5')
+const numCount = document.querySelector("#numberCount");
+
+let int = document.getElementById('counter')
+let integer = 0
+
+minus1.addEventListener('click', function () {
+  integer -= 1
+  int.innerHTML = integer
+})
+
+minus5.addEventListener('click', function () {
+  integer -= 5
+  int.innerHTML = integer
+})
+
+reset.addEventListener('click', function () {
+  integer = 0
+  int.innerHTML = integer
+})
+
+add1.addEventListener('click', function () {
+  integer += 1
+  int.innerHTML = integer
+})
+
+add5.addEventListener('click', function () {
+  integer += 5
+  int.innerHTML = integer
+})
